c25b1f29863c851bce773a3776e4de06141d453d
Eric Onuki <Eric.Onuki@wwecorp.com>
2018-05-26 17:35:30 -0400

    Adds bouncer migrations

---
+++ database/migrations/2018_05_26_213507_create_bouncer_tables.php
@@ -0,0 +1,88 @@
+<?php
+
+use Silber\Bouncer\Database\Models;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBouncerTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(Models::table('abilities'), function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 150);
+            $table->string('title')->nullable();
+            $table->integer('entity_id')->unsigned()->nullable();
+            $table->string('entity_type', 150)->nullable();
+            $table->boolean('only_owned')->default(false);
+            $table->integer('scope')->nullable()->index();
+            $table->timestamps();
+        });
+
+        Schema::create(Models::table('roles'), function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 150);
+            $table->string('title')->nullable();
+            $table->integer('level')->unsigned()->nullable();
+            $table->integer('scope')->nullable()->index();
+            $table->timestamps();
+
+            $table->unique(
+                ['name', 'scope'],
+                'roles_name_unique'
+            );
+        });
+
+        Schema::create(Models::table('assigned_roles'), function (Blueprint $table) {
+            $table->integer('role_id')->unsigned()->index();
+            $table->integer('entity_id')->unsigned();
+            $table->string('entity_type', 150);
+            $table->integer('scope')->nullable()->index();
+
+            $table->index(
+                ['entity_id', 'entity_type', 'scope'],
+                'assigned_roles_entity_index'
+            );
+
+            $table->foreign('role_id')
+                  ->references('id')->on(Models::table('roles'))
+                  ->onUpdate('cascade')->onDelete('cascade');
+        });
+
+        Schema::create(Models::table('permissions'), function (Blueprint $table) {
+            $table->integer('ability_id')->unsigned()->index();
+            $table->integer('entity_id')->unsigned();
+            $table->string('entity_type', 150);
+            $table->boolean('forbidden')->default(false);
+            $table->integer('scope')->nullable()->index();
+
+            $table->index(
+                ['entity_id', 'entity_type', 'scope'],
+                'permissions_entity_index'
+            );
+
+            $table->foreign('ability_id')
+                  ->references('id')->on(Models::table('abilities'))
+                  ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop(Models::table('permissions'));
+        Schema::drop(Models::table('assigned_roles'));
+        Schema::drop(Models::table('roles'));
+        Schema::drop(Models::table('abilities'));
+    }
+}
