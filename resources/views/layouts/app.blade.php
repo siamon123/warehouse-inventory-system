759e8beb81d89da742b0a4bd16488d7894660d95
Eric Onuki <Eric.Onuki@wwecorp.com>
2018-05-27 17:29:15 -0400

    add csrf token and stylesheet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
   <title>Inventory Management System - @yield('title')</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
   @yield('content')
