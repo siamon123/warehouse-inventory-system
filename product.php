860f9406bc80b92339b83dd6ca8645302e45819c
Michael C Tiernan <Michael.Tiernan@gMail.com>
2020-03-21 15:08:18 -0400

    Unmade my mess.

--- product.php
+++ product.php
@@ -24,12 +24,12 @@
                 <th class="text-center" style="width: 50px;">#</th>
                 <th> Photo</th>
                 <th> Product Title </th>
-                <th class="text-center" style="width: 10%;">Category</th>
-                <th class="text-center" style="width: 10%;">In stock </th>
-                <th class="text-center" style="width: 10%;">Cost</th>
-                <th class="text-center" style="width: 10%;">Selling Price</th>
-                <th class="text-center" style="width: 10%;">Product Added</th>
-                <th class="text-center" style="width: 100px;">Actions</th>
+                <th class="text-center" style="width: 10%;"> Categorie </th>
+                <th class="text-center" style="width: 10%;"> Instock </th>
+                <th class="text-center" style="width: 10%;"> Buying Price </th>
+                <th class="text-center" style="width: 10%;"> Saleing Price </th>
+                <th class="text-center" style="width: 10%;"> Product Added </th>
+                <th class="text-center" style="width: 100px;"> Actions </th>
               </tr>
             </thead>
             <tbody>
