b3e7241950e7381d20ec7f33ce729dd1df09d3e9
Michael Tiernan <Michael.Tiernan@gMail.com>
2020-03-22 14:09:46 -0400

    Correction to allow special user not just admin to control categories.

--- categorie.php
+++ categorie.php
@@ -2,7 +2,7 @@
   $page_title = 'All categories';
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
-  page_require_level(1);
+  page_require_level(2);
   
   $all_categories = find_all('categories')
 ?>
